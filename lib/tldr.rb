a7ebe89fbb9d9afb68cb19a12f23bf7b4fbde82a
Peter Singh <singh.prd@gmail.com>
2020-09-23 14:13:31 +0100

    more work

--- lib/tldr.rb
+++ lib/tldr.rb
@@ -6,15 +6,19 @@ require 'tty-markdown'
 module Tldr
   REPO_PATH = File.expand_path('~/.cache/')
   REPO_NAME = "freeagent_til"
-  FULL_PATH = File.join(REPO_PATH, REPO_NAME)
   REJECT_LIST = [".", "..", ".git"]
+
 
   class Error < StandardError; end
 
   class Setup
+	  def full_path
+	  	File.join(REPO_PATH, REPO_NAME)
+	  end
+
   	def self.update_cache
 			if Dir.exists?(FULL_PATH)  		
-				g = Git.open(Tldr::FULL_PATH)
+				g = Git.open(full_path)
 				g.fetch
 				g.pull
 			else
@@ -24,26 +28,27 @@ module Tldr
   end
 
   class Runner
-	  def self.fetch_entry
-	  	new().get_page
+	  def self.fetch_entry(til_area, til_snippet)
+	  	path_to_entry = new().til_entry_for(til_area, til_snippet)
+
+	  	title = TTY::Markdown.parse("# FreeAgent TIL O'the day: Spotlight")
+	  	parsed = TTY::Markdown.parse_file(path_to_entry)
+	  	return "-------\n\n#{title}\n\n" + parsed + "\n-------\n\n"
 	  end
 
+  	def full_path
+  		File.join(REPO_PATH, REPO_NAME)
+  	end
+
 	  def til_areas
-	  	Dir.entries(Tldr::FULL_PATH).reject { |folder| REJECT_LIST.include?(folder) }
+	  	Dir.entries(full_path).reject { |folder| REJECT_LIST.include?(folder) }
 	  end
 
 	  def til_entry_for(til_area, til_snippet)
-	  	entries = Dir.entries(File.join(Tldr::FULL_PATH, til_area)).reject { |folder| REJECT_LIST.include?(folder) }
+	  	entries = Dir.entries(File.join(full_path, til_area)).reject { |folder| REJECT_LIST.include?(folder) }
 
-	  	path_to_entry = File.join(Tldr::FULL_PATH, til_area, til_snippet)
+	  	path_to_entry = File.join(full_path, til_area, til_snippet)
 	  	path_to_entry
-	  end
-
-	  def get_page
-	  	title = TTY::Markdown.parse("# FreeAgent TIL O'the day: Spotlight")
-	  	parsed = TTY::Markdown.parse_file(til_entry_for("OS X", "spotlight.md"))
-	  	puts "\n\n#{title}\n\n"
-	  	puts parsed
 	  end
   end
 end
--- spec/tldr_spec.rb
+++ spec/tldr_spec.rb
@@ -1,9 +1,17 @@
 RSpec.describe Tldr do
-  it "has a version number" do
-    expect(Tldr::VERSION).not_to be nil
-  end
+	before(:each) do
+		stub_const("Tldr::REPO_PATH", "spec/fixtures")
+	end
+
+  # it "has a version number" do
+  #   expect(Tldr::VERSION).not_to be nil
+  # end
 
   it "does something useful" do
     expect(Tldr::Runner.fetch_entry).to eq("hello world")
+  end
+
+  it "it has a path" do
+    expect(Tldr::REPO_PATH).to eq("spec/fixtures")
   end
 end
